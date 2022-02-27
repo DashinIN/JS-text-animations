97cac0d35da4043e50399e87ae8dd701c4a18cee
DashinIN <green19grant@yandex.ru>
2022-02-27 17:47:19 +0300

    адаптив и анимации

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,19 @@
 
 
 
-const animItems = document.querySelectorAll('._anim-items');
+const animItems = document.querySelectorAll('._anim-items'); // получаем колекцию элементов с классом _anim-items
 
 
-if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll() {
-       for(let element of animItems) {
+if (animItems.length > 0) { //Если длинна такой коллекции больше 0 (такие элементы на странице есть)
+    window.addEventListener('scroll', animOnScroll); // То при событии прокрутки выполняем эту функцию:
+    function animOnScroll() { 
+       for(let element of animItems) { // Для кажжого элемента этой коллекции
         const animItem = element;
-        console.log(animItem);
-        const animItemHeight = animItem.offsetHeight;
-        console.log(animItemHeight);
-        const animItemOffset = offset(animItem).top;
-        console.log(animItemOffset);
-        const animStart = 4;
+        const animItemHeight = animItem.offsetHeight; // Получаем его высоту и позицию относительно верха
+        const animItemOffset = offset(animItem).top; 
+        const animStart = 2;
   
-        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        let animItemPoint = window.innerHeight -  animItemHeight / animStart;
 
         if (animItemHeight > window.innerHeight) {
             animItemPoint = window.innerHeight - window.innerHeight / animStart;
@@ -26,18 +23,21 @@ if (animItems.length > 0) {
         if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
             animItem.classList.add('_active')
         } else {
+            if (!animItem.classList.contains('_anim-no-hide')){
             animItem.classList.remove('_active')
+         }
         }
 
        }
     }
     
     function offset(el) {
-        const rect = el.getBoundingClientRect(),
-        scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-        scrollTop =  window.scrollY || document.documentElement.scrollTop;
-        return {
-            top: rect.top + scrollTop, left: rect.left + scrollLeft 
-        }
+        const rect = el.getBoundingClientRect(), // Получаем позицию элемента относительно viewport
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop =  window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft } // Возвращает объект чтобы потом выбрать сторону
     }
+   setTimeout(() => {
+    animOnScroll();
+   }, 300);
 }
